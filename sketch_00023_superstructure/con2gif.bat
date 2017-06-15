9a4a95859e3f50382cd55bff3d0f15014adeab3c
Max Farrell <maxleonardfarrell1@gmail.com>
2017-06-15 09:20:06 -0400

    Clean up

--- sketch_00023_superstructure/con2gif.bat
+++
@@ -1,2 +0,0 @@
-ffmpeg -f image2 -i frame_%%05d.png video.avi
-ffmpeg -i video.avi -pix_fmt rgb24 out.gif
--- sketch_00034_rain/con2gif.bat
+++
@@ -1,2 +0,0 @@
-ffmpeg -f image2 -i frame_%%05d.jpg video.avi
-ffmpeg -i video.avi -pix_fmt rgb24 out.gif
